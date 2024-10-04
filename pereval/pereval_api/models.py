bc83fae77d4aba2a72e5301683d0051cec6af276
danpilnet <dnpdnp28@gmail.com>
2024-10-04 23:03:50 +0900

    add_models

--- pereval/pereval_api/models.py
+++ pereval/pereval_api/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+class UserPereval(models.Model):
+    full_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    pnone = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+
 class Status(models.Model):
 
     NEW = 'NW'
@@ -15,4 +21,16 @@ class Status(models.Model):
 
     ]
 
+    user = models.ForeignKey(UserPereval, on_delete=models.CASCADE)
     status = models.CharField(max_length=2, choices=STATUS_CHOICES, default=NEW)
+
+class Pereval(models.Model):
+
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(UserPereval, on_delete=models.CASCADE)
+    text = models.TextField(max_length=400)
+
+class Coordinates(models.Model):
+    longitude = models.DecimalField(max_digits=10, decimal_places=8, unique=True)
+    latitude = models.DecimalField(max_digits=10, decimal_places=8, unique=True)
+    height = models.DecimalField(max_digits=10, decimal_places=8, unique=True)
