d5e9f86a049a27a13c6a7d21ee3da9cf9d196366
danpilnet <dnpdnp28@gmail.com>
2024-10-04 23:46:19 +0900

    add_views

--- pereval/pereval_api/views.py
+++ pereval/pereval_api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import UserPereval, Status, Pereval, Coordinates
+from .serializers import UserSerializaer, StatusSerializers, PerevalSerializers, CoordinatesSerializers
+
+
+class UserApiView(viewsets.ModelViewSet):
+    queryset = UserPereval.objects.all()
+    serializer_class = UserSerializaer
+
+class StatusApiView(viewsets.ModelViewSet):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializers
+
+class PerevalApiView(viewsets.ModelViewSet):
+    queryset = Pereval
+    serializer_class = PerevalSerializers
+
+class Coordinates(viewsets.ModelViewSet):
+    queryset = Coordinates
+    serializer_class = CoordinatesSerializers
